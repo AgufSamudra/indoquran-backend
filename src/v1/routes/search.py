7a1043a049da65f6cf9b2acdc8fe2ba839eeeb6f
Administrator <gufranaksamudra2003@gmail.com>
2025-02-14 23:05:11 +0700

    add 3 field in api response

--- src/v1/routes/search.py
+++ src/v1/routes/search.py
@@ -29,26 +29,31 @@ async def smart_search(input: SmartSearch):
         # Buat embedding dari input user
         user_vector = model.encode(input.user_input).tolist()
 
+        # Query untuk Tafsir dengan threshold < 0.5
         cursor.execute("""
-            SELECT number, name, slug, arab, teks
-            FROM detail_hadist_embedding
-            ORDER BY vector <=> %s::vector
-            LIMIT 10;
-        """, (user_vector,))
-        hadist_results = [dict(row) for row in cursor.fetchall()]
-
-        # Query untuk Tafsir
-        cursor.execute("""
-            SELECT nomor, nama, nama_latin, jumlah_ayat, tempat_turun, arti, ayat, teks
+            SELECT 
+                nomor,
+                nama,
+                nama_latin,
+                jumlah_ayat,
+                tempat_turun,
+                arti,
+                ayat,
+                teks,
+                teks_arab,
+                teks_latin,
+                teks_indonesia,
+                vector <=> %s::vector AS distance_score
             FROM detail_tafsir_embedding
-            ORDER BY vector <=> %s::vector
+            WHERE (vector <=> %s::vector) < 0.5       -- threshold
+            ORDER BY distance_score
             LIMIT 10;
-        """, (user_vector,))
+        """, (user_vector, user_vector))
+        
         tafsir_results = [dict(row) for row in cursor.fetchall()]
 
-        # Return hasil pencarian
+        # Return hasil pencarian (hanya tafsir)
         return {
-            "hadist": hadist_results,
             "tafsir": tafsir_results
         }
 
